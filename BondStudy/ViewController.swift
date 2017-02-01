af6176efdb36b558ff3176c09487cc97e6bc70df
DaichiSaito <h1a_ble@yahoo.co.jp>
2017-02-01 17:03:35 +0900

    Initial Commit

---
+++ BondStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BondStudy
+//
+//  Created by DaichiSaito on 2017/02/01.
+//  Copyright © 2017年 DaichiSaito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
